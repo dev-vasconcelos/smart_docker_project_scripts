0e48e07525fe15e32adc7d2a64b45e6dce380b37
pedrobipede <pedroivomartinsdevasconcelos@gmail.com>
2021-08-02 16:42:40 -0300

    project handler encapsulation

--- project_handler_command/phandler_command.sh
+++ project_handler_command/phandler_command.sh
@@ -1,8 +1,10 @@
-DEFAULTPROJECTFOLDER="/home/predogalahad/Documents/Projects"
+DEFAULTPROJECTFOLDER="$(echo ~)/Documents/Projects"
 
-projeto() {
+projeto() {(
+	functions_absolute_path="$(echo ~)/phandler_function.sh"
+	. $functions_absolute_path
+	
 	funcao=$1
-	# nome_projeto=$2	
 	current=$(pwd)
 	while getopts f:w:b:o: flags
 	do
@@ -32,6 +34,7 @@ projeto() {
 			*) echo "Funcao não encontrada! Fechando!"; teste;break;;
 		esac
 	done
+
 	unset funcao
 	unset nome_projeto
 	unset workspace
@@ -41,46 +44,11 @@ projeto() {
 	# git_handler $funcao $nome_projeto $path $branch
 
 	
-}
+	)}
 
-doubt() {
-	echo sera que isso é elegante de fazer? uma funcao dentro de outra pra capsulas
-	echo code on function inside another to encapsulate, is it elegant... or even good?
-	echo when I discover, if it is will keep this way
-	echo if not, then put the functions outside this file
-}
-	
-docker_selfrun() {
-	current_dir=$(pwd)
-	if [ "$workspace" == "$DEFAULTPROJECTFOLDER" ]; then
-	cd $DEFAULTPROJECTFOLDER && ./selfrun.sh -f $project_folder -j
-	else
-		cd $path && cd .. && ./selfrun.sh -f $project_folder -j
-	fi
-	cd $current_dir
-}
-
-git_up() {
-	branch=$(git -C $path symbolic-ref --short HEAD)
-	git -C $path add .
-		read -r -p "Entre a mensagem de commit: " message
-	git -C $path commit -m "$message"
-		read -r -p "Confirmar commi no brach $branch [y/N]" confirm
-	echo $confirm
-
-	if [[ "$confirm" =~ ^([yY][eE][sS]|[yY])$ ]]
-	then
-		git -C $path push -u origin $branch 
-	else
-		echo "Push cancelado"
-	fi
-}
 
 complete -W "-f -n -w -b -o -h \
 	--funcao --nome --workspace --branch --folder --help \
 	rodar deploy atualizar status checkout \
         $(ls $DEFAULTPROJECTFOLDER)" projeto
-
-
-
 
---
+++ project_handler_command/phandler_function.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+doubt() {
+	echo sera que isso é elegante de fazer? uma funcao dentro de outra pra capsulas
+	echo code on function inside another to encapsulate, is it elegant... or even good?
+	echo when I discover, if it is will keep this way
+	echo if not, then put the functions outside this file
+}
+	
+docker_selfrun() {
+	current_dir=$(pwd)
+	if [ "$workspace" == "$DEFAULTPROJECTFOLDER" ]; then
+	cd $DEFAULTPROJECTFOLDER && ./selfrun.sh -f $project_folder -j
+	#. $DEFAULTPROJECTFOLDER/selfrun.sh -f $project_folder -j
+	else
+		cd $path && cd .. && ./selfrun.sh -f $project_folder -j
+	fi
+	cd $current_dir
+}
+
+git_up() {
+	branch=$(git -C $path symbolic-ref --short HEAD)
+	git -C $path add .
+		read -r -p "Entre a mensagem de commit: " message
+	git -C $path commit -m "$message"
+		read -r -p "Confirmar commi no brach $branch [y/N]" confirm
+	echo $confirm
+
+	if [[ "$confirm" =~ ^([yY][eE][sS]|[yY])$ ]]
+	then
+		git -C $path push -u origin $branch 
+	else
+		echo "Push cancelado"
+	fi
+}
